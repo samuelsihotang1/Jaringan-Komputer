63bf7b02cd4db770a592719347131c7774ac7b46
Christina <iss21041@students.del.ac.id>
2023-05-12 11:55:40 +0700

    Enkripsi
    Berhasil mengenkripsi pesan

--- Client2.java
+++ Client2.java
@@ -13,7 +13,9 @@ public class Client2 {
     String userInput = null;
     String output = null;
     String mode = null;
-    int TESTPORT = 1;
+    int targetport;
+    String encrypteduserInput = null;
+    String decryptedString = null;
 
     // Determine mode: server or client
     if (args.length > 0 && args[0].equals("server")) {
@@ -28,8 +30,8 @@ public class Client2 {
       try {
         System.out.print("ID Anda: ");
         String str = stdin.readLine();
-        TESTPORT = str.codePoints().sum();
-        checkServer = new ServerSocket(TESTPORT);
+        targetport = str.codePoints().sum();
+        checkServer = new ServerSocket(targetport);
         clientSocket = checkServer.accept();
         is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         os = new DataOutputStream(clientSocket.getOutputStream());
@@ -49,24 +51,28 @@ public class Client2 {
         if (mode.equals("client")) {
           System.out.print("Anda: ");
           userInput = stdin.readLine();
-          os.writeBytes(userInput + "\n");
+          encrypteduserInput = encryptString(userInput);
+          os.writeBytes(encrypteduserInput + "\n");
           if (userInput.equalsIgnoreCase("exit")) {
             break;
           }
           output = is.readLine();
-          System.out.println("Teman: " + output);
-          if (output.equalsIgnoreCase("exit")) {
+          decryptedString = decryptString(output);
+          System.out.println("Teman: " + decryptedString);
+          if (decryptedString.equalsIgnoreCase("exit")) {
             break;
           }
         } else if (mode.equals("server")) {
           output = is.readLine();
-          System.out.println("Teman: " + output);
-          if (output.equalsIgnoreCase("exit")) {
+          decryptedString = decryptString(output);
+          System.out.println("Teman: " + decryptedString);
+          if (decryptedString.equalsIgnoreCase("exit")) {
             break;
           }
           System.out.print("Anda: ");
           userInput = stdin.readLine();
-          os.writeBytes(userInput + "\n");
+          encrypteduserInput = encryptString(userInput);
+          os.writeBytes(encrypteduserInput + "\n");
           if (userInput.equalsIgnoreCase("exit")) {
             break;
           }
@@ -121,4 +127,25 @@ public class Client2 {
     System.out.println("Kamu telah terhubung");
     return cl;
   }
+
+  public static String encryptString(String input) {
+    StringBuilder encrypted = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      char c = input.charAt(i);
+      c += 1 + i;
+      encrypted.append(c);
+    }
+    return encrypted.toString();
+  }
+
+  public static String decryptString(String input) {
+    StringBuilder decrypted = new StringBuilder();
+    for (int i = 0; i < input.length(); i++) {
+      char c = input.charAt(i);
+      c -= 1 + i;
+      decrypted.append(c);
+    }
+    return decrypted.toString();
+  }
+
 }
