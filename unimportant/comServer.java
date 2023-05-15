5fa163dcd2521c5c6835aa18e80c59c7954b9106
Christina <iss21041@students.del.ac.id>
2023-05-15 20:57:05 +0700

    Merapikan file

---
+++ unimportant/comServer.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.net.*;
+
+public class comServer {
+  public final static int TESTPORT = 424;
+
+  public static void main(String[] args) {
+    ServerSocket checkServer = null;
+    String line;
+    BufferedReader is = null;
+    DataOutputStream os = null;
+    Socket clientSocket = null;
+    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    String userInput = null;
+
+    // Start server
+    try {
+      checkServer = new ServerSocket(TESTPORT);
+      System.out.println("Aplikasi server hidup dengan ID: " + TESTPORT);
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+    // Start server
+
+    try {
+      clientSocket = checkServer.accept();
+      is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      os = new DataOutputStream(clientSocket.getOutputStream());
+
+      while (true) {
+        line = is.readLine();
+        System.out.println("Dari client: " + line);
+        if (line.equalsIgnoreCase("exit")) {
+          break;
+        }
+        System.out.print("Masukkan pesan: ");
+        userInput = stdin.readLine();
+        os.writeBytes(userInput.toString() + "\n");
+        if (userInput.equalsIgnoreCase("exit")) {
+          break;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    try {
+      os.close();
+      is.close();
+      clientSocket.close();
+      checkServer.close();
+    } catch (IOException ic) {
+      ic.printStackTrace();
+    }
+  }
+}
---
+++ unimportant/time.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class time {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Waktu saat ini: " + date.toString());
+    }
+}
