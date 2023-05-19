fba530bc69e307003c67f8833bcc8b91e82ce2b5
Christina <iss21041@students.del.ac.id>
2023-05-19 11:15:32 +0700

    Menambahkan comment

--- Group Chat/Client.java
+++ Group Chat/Client.java
@@ -1,41 +1,59 @@
 import java.io.*;
 import java.net.*;
 
-public class cl {
+public class Client {
   public static void main(String[] args) throws IOException {
+    // Membuat objek BufferedReader untuk membaca input dari keyboard
     BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    // Menentukan apakah program ini akan berperan sebagai pengirim atau penerima
     if (args.length != 0 && args[0].equals("sender")) {
       System.out.print("Masukkan kata kunci: ");
+      // Membaca input dari keyboard
       String str = stdin.readLine();
+      // Menjumlahkan nilai ASCII dari setiap karakter pada string
       int port = str.codePoints().sum();
       
+      // Membuat koneksi ke server
       Socket socket = new Socket("localhost", port);
+      // Membuat objek PrintWriter untuk menulis ke socket
       PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
       System.out.print("ID Anda: ");
+      // Meminta input userID dari keyboard
       String userID = stdin.readLine();
+      // Mengirim userID ke server
       out.println(userID);
 
       while (true) {
         System.out.print("> ");
+        // Meminta input pesan dari keyboard
         String message = stdin.readLine();
+        // Jika pesan yang dikirim adalah "---", maka program akan berhenti
         if (message.equals("---")) {
           break;
         }
+        // Mengirim pesan ke server
         out.println(message);
       }
       socket.close();
     } else if (args.length != 0 && args[0].equals("receiver")) {
       System.out.print("Masukkan kata kunci: ");
+      // Membaca input dari keyboard
       String str = stdin.readLine();
+      // Menjumlahkan nilai ASCII dari setiap karakter pada string
       int port = str.codePoints().sum();
 
+      // Membuat koneksi ke server
       Socket socket = new Socket("localhost", port);
+      // Membuat objek BufferedReader untuk membaca dari socket
       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
       while (true) {
+        // Membaca pesan dari server
         String response;
+        // Jika pesan yang diterima tidak null, maka program akan menampilkan pesan
         while ((response = in.readLine()) != null) {
+          // Menampilkan pesan
           System.out.println(response);
           break;
         }
--- Group Chat/Server.java
+++ Group Chat/Server.java
@@ -4,47 +4,69 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class se {
+public class Server {
+  // Membuat list untuk mengumpulkan client
   private static List<Socket> clients = new ArrayList<Socket>();
 
   public static void main(String[] args) throws IOException {
+    // Membuat scanner untuk input
     Scanner scanner = new Scanner(System.in);
+    // Menerima input dari user
     System.out.print("Masukkan kata kunci: ");
     String str = scanner.nextLine();
+    // Menjumlahkan ASCII dari kata kunci
     int port = str.codePoints().sum();
+    // Membuat server socket berdasarkan jumlah ASCII
     ServerSocket server = new ServerSocket(port);
 
     while (true) {
+      // Menerima koneksi dari client
       Socket client = server.accept();
+      // Menambahkan client ke list
       clients.add(client);
-      Thread t = new Thread(new ClientHandler(client));
-      t.start();
+      // Membuat thread untuk client
+      Thread threadClient = new Thread(new pengurusPengguna(client));
+      // Menjalankan thread
+      threadClient.start();
     }
   }
 
-  private static class ClientHandler implements Runnable {
+  private static class pengurusPengguna implements Runnable {
+    // Membuat socket untuk client
     private Socket client;
+    // Membuat variabel untuk menyimpan userID
     private String userID;
+    // Membuat variabel untuk menyimpan waktu dengan format HH:mm:ss
     SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 
-    public ClientHandler(Socket client) {
+    // Constructor
+    public pengurusPengguna(Socket client) {
       this.client = client;
     }
 
     public void run() {
       try {
+        // Membuat buffer untuk membaca input dari client
         BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        // Membaca userID dari client
         userID = in.readLine();
         while (true) {
+          // Membaca pesan dari client
           String message = in.readLine();
+          // Jika pesan kosong, maka client akan dihapus dari list
           if (message == null) {
             clients.remove(client);
             break;
           } else {
+            // Jika pesan tidak kosong, maka pesan akan dikirim ke semua client
             for (Socket Client : clients) {
+              // Membuat buffer untuk mengirim pesan ke client
               PrintWriter out = new PrintWriter(Client.getOutputStream(), true);
+              // Membuat variabel untuk menyimpan waktu saat ini
               Date date = new Date();
+              // Mengubah waktu saat ini menjadi string dengan format HH:mm:ss
               String currentTime = formatter.format(date);
+              // Mengirim pesan ke client
               out.println("[" + userID + "]'" + currentTime + "' > " + message);
             }
           }
