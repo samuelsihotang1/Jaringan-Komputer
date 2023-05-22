71f01080a2e2da20a6a6bf555d40c06abfd1c7d8
Christina <iss21041@students.del.ac.id>
2023-05-22 17:53:56 +0700

    Menambahi Comment

--- Client-to-Client Chat/Chat.java
+++ Client-to-Client Chat/Chat.java
@@ -4,6 +4,7 @@ import java.net.*;
 public class Chat {
 
   public static void main(String args[]) throws Exception {
+    // Deklarasi Variabel
     ServerSocket checkServer = null;
     Socket clientSocket = null;
     Socket cl = null;
@@ -17,7 +18,7 @@ public class Chat {
     String encrypteduserInput = null;
     String decryptedString = null;
 
-    // Determine mode: server or client
+    // Menentukan Mode Server atau Client
     if (args.length > 0 && args[0].equals("server")) {
       mode = "server";
     }
@@ -25,21 +26,28 @@ public class Chat {
       mode = "client";
     }
 
-    // Start server or connect to server
+    // Jika mode server, maka buat server socket
     if (mode.equals("server")) {
       try {
         System.out.print("ID Anda: ");
+        // Membaca input dari user
         String str = stdin.readLine();
+        // Menghitung jumlah karakter dari input user
         targetport = str.codePoints().sum();
+        // Membuat server socket
         checkServer = new ServerSocket(targetport);
+        // Menunggu koneksi dari client
         clientSocket = checkServer.accept();
+        // Membuat input dan output stream
         is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         os = new DataOutputStream(clientSocket.getOutputStream());
         System.out.println("Kamu telah terhubung");
       } catch (IOException e) {
         System.out.println(e);
       }
+      // Jika mode client, maka buat client socket
     } else {
+      // Membuat koneksi ke server
       cl = connectToServer(stdin);
       is = new BufferedReader(new InputStreamReader(cl.getInputStream()));
       os = new DataOutputStream(cl.getOutputStream());
@@ -50,29 +58,44 @@ public class Chat {
       while (true) {
         if (mode.equals("client")) {
           System.out.print("Anda: ");
+          // Membaca input dari user
           userInput = stdin.readLine();
+          // Mengenkripsi input user
           encrypteduserInput = encryptString(userInput);
+          // Mengirimkan input user ke server
           os.writeBytes(encrypteduserInput + "\n");
+          // Jika input user adalah exit, maka keluar dari loop
           if (userInput.equalsIgnoreCase("exit")) {
             break;
           }
+          // Menerima pesan dari server
           output = is.readLine();
+          // Mendekripsi pesan dari server
           decryptedString = decryptString(output);
           System.out.println("Teman: " + decryptedString);
+          // Jika pesan dari server adalah exit, maka keluar dari loop
           if (decryptedString.equalsIgnoreCase("exit")) {
             break;
           }
+          // Jika mode server, maka menerima pesan dari client
         } else if (mode.equals("server")) {
+          // Menerima pesan dari client
           output = is.readLine();
+          // Mendekripsi pesan dari client
           decryptedString = decryptString(output);
           System.out.println("Teman: " + decryptedString);
+          // Jika pesan dari client adalah exit, maka keluar dari loop
           if (decryptedString.equalsIgnoreCase("exit")) {
             break;
           }
           System.out.print("Anda: ");
+          // Membaca input dari user
           userInput = stdin.readLine();
+          // Mengenkripsi input user
           encrypteduserInput = encryptString(userInput);
+          // Mengirimkan input user ke client
           os.writeBytes(encrypteduserInput + "\n");
+          // Jika input user adalah exit, maka keluar dari loop
           if (userInput.equalsIgnoreCase("exit")) {
             break;
           }
@@ -82,7 +105,7 @@ public class Chat {
       e.printStackTrace();
     }
 
-    // Close connection
+    // Menutup koneksi
     try {
       is.close();
       os.close();
@@ -97,8 +120,9 @@ public class Chat {
     }
   }
 
-  // Function to Connect to Server
+  // Fungsi untuk membuat koneksi ke server
   public static Socket connectToServer(BufferedReader stdin) throws IOException {
+    // Deklarasi Variabel
     Socket cl = null;
     boolean again = false;
     int REMOTE_PORT;
@@ -110,9 +134,11 @@ public class Chat {
         } else {
           System.out.print("Masukkan ID Target: ");
         }
-
+        // Membaca input dari user
         String str = stdin.readLine();
+        // Menghitung jumlah karakter dari input user
         REMOTE_PORT = str.codePoints().sum();
+        // Membuat koneksi ke server
         cl = new Socket("localhost", REMOTE_PORT);
         again = false;
       } catch (UnknownHostException el) {
@@ -129,23 +155,28 @@ public class Chat {
   }
 
   public static String encryptString(String input) {
+    // Deklarasi Variabel
     StringBuilder encrypted = new StringBuilder();
+    // Mengenkripsi input user
     for (int i = 0; i < input.length(); i++) {
       char c = input.charAt(i);
       c += 1 + i;
       encrypted.append(c);
     }
+    // Mengembalikan nilai input yang telah dienkripsi
     return encrypted.toString();
   }
 
   public static String decryptString(String input) {
+    // Deklarasi Variabel
     StringBuilder decrypted = new StringBuilder();
+    // Mendekripsi input user
     for (int i = 0; i < input.length(); i++) {
       char c = input.charAt(i);
       c -= 1 + i;
       decrypted.append(c);
     }
+    // Mengembalikan nilai input yang telah didekripsi
     return decrypted.toString();
   }
-
 }
